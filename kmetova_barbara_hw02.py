3e31a45f4fe98c493b80361ae6b66182235e245c
Barbara <barbara@ventrata.com>
2026-05-22 13:53:59 +0200

    Pridanie funkcie a vymazanie medzery pred "cast", "genres" a "directors"

--- kmetova_barbara_hw02.py
+++ kmetova_barbara_hw02.py
@@ -1,12 +1,20 @@
 import json
 
+# Tu si podchytíme prázdne hodnoty v jednotlivých stĺpcoch, aby v prípade, že hodnota na jednotlivých filmoch chýba, tak sa vytvorí v slovníku prázdny zoznam a for cyklus ju nepreskočí. 
+# Pre zjednodušenie som vytvorila funkciu do ktorej som vložila všetky atribúty, kde je podmienka rovnaká.    
+def empty_value(value):
+    if value == "":
+        return []
+    else:
+        return [item.strip() for item in value.split(",")]
+
 # Vytvoríme si prázdne zoznamy. Lines potrebujeme na čítanie súboru a movie_list na konečný výstup, do ktorého potom vložíme slovníky s filmami.
 lines = []
 movie_list = []
 
 # Otvoríme súbor, zadáme kódovanie na podporu diakritiky a čítame po riadkoch.
 with open('netflix_titles.tsv', encoding='utf-8') as file:
-    lines = file.readlines()
+    lines = file.readlines() 
 
 # Vytvoríme for cyklus, ktorým si rozdelíme hodnoty do stĺpcov. Keďže súbor je tsv, tak hodnoty oddelujeme tabulátorom ("\t"). Vytiahneme si stĺlpce, ktoré budeme potrebovať do finálneho súboru podľa indexu.
 for line in lines[1:]: 
@@ -16,22 +24,11 @@ for line in lines[1:]:
     cast = columns[16]
     genres = columns[8]
 # Dekádu vypočítame tak, že rok prevedieme na číslo, vydelíme 10 aby sme mohli zaokrúhliť číslo a dostať rok len s desaťročím a potom vynásobíme naspät 10.     
-    decade = int(columns[5]) // 10 * 10
-# Tu si podchytíme prázdne hodnoty v jednotlivých stĺpcoch, aby v prípade, že hodnota na jednotlivých filmoch chýba, tak sa vytvorí v slovníku prázdny zoznam a for cyklus ju nepreskočí. 
-    if directors == "":
-        directors = []
-    else:
-        directors = directors.split(",")    
+    decade = int(columns[5]) // 10 * 10 
     
-    if cast == "":
-        cast = []
-    else:
-        cast = cast.split(",")    
-
-    if genres == "":
-        genres = []
-    else:
-        genres = genres.split(",")    
+    directors = empty_value(directors)
+    cast = empty_value(cast)
+    genres = empty_value(genres)  
 
 # Vytvoríme si slovník, do ktorého vložíme jednotlivé hodnoty zadefinované predtým.        
     movie = {
@@ -42,7 +39,7 @@ for line in lines[1:]:
         "decade": decade
     }
     movie_list.append(movie)
-   
+
 # Uložíme si súbor vo formáte json. 
 with open('kmetova_barbara_hw_02.json', mode='w', encoding='utf-8') as output_file:
     json.dump(movie_list, output_file, indent=4, ensure_ascii=False)
